e0bf766a01382e0ae0ee75080eb02e192c6c4dc2
mohammedmuk <mohammedmuk20@gmail.com>
2023-07-03 22:52:27 +0200

    add 0x00-pascal_triangle directory with two files (/README.md, 0-pascal_triangle.py)

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    Create a function def pascal_triangle(n)
+"""
+
+
+def pascal_triangle(n):
+    """
+        returns a list of lists of
+        integers representing the
+        Pascals triangle of n.
+    """
+
+    triangle_list = ['1']
+    main_list = []
+    # Copy from triangle_list
+    copy_list = []
+
+    main_list.append(triangle_list.copy())
+    if n <= 0:
+        return main_list
+
+    for i in range(n - 1):
+        triangle_list.append('0')
+        copy_list = triangle_list.copy()
+        copy_list.reverse()
+
+        for index in range(len(triangle_list)):
+            triangle_list[index] =
+            str(int(triangle_list[index]) + int(copy_list[index]))
+
+        main_list.append(triangle_list.copy())
+
+    # return list of lists.
+    return main_list
