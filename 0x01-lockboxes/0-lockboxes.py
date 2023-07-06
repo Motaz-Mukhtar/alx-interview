2f3cd86c3a260908df8ccd46d6dc97ba75f56ccb
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-06 15:50:27 -0700

    another modify (PEP style) at 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,24 @@
 #!usr/bin/python3
 """
-  Lock Boxes
+    Lock Boxes
 """
 
 
 def canUnlockAll(boxes):
-	"""
-	Determine if all the boxes
-	can opened.
-	"""
-	opened_boxes = []
-	opened_boxes.appendboxes[0])
+    """
+    Determine if all boxes can
+    be opened
+    """
+    opened_boxes = []
+    opened_boxes.append(boxes[0])
 
-	if boxes[0] == []:
-		return False
+    for box in opened_boxes:
+        for key in box:
+            if boxes[key] in opened_boxes:
+                continue
+            opened_boxes.append(boxes[key])
 
-	for box in opened_boxes:
-		for key in box:
-			if boxes[key] in opened_boxes:
-				continue
-			opened_boxes.append(boxes[key])
-	if len(boxes) == len(opened_boxes):
-		return True
-	else:
-		return False
+    if len(boxes) == len(opened_boxes):
+        return True
+    else:
+        return False
