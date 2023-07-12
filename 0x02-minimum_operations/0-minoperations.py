b63d7eb41cb8624be75e52db393c2eaf21c9e817
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-13 01:02:14 +0200

    remove 'num' variable and add 'if' clauses

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,20 +14,26 @@ def minOperations(n: int) -> int:
     copy_list = []
     number_of_operations = 0
 
-    #if n <= 0:
-     #   return 0
+    if n <= 0:
+        return 0
 
-    num = 0
+    if n == 1:
+        return 0
+
+    if n == 2:
+        return 2
+
+    if n == 3:
+        return 3
+
     while True:
-        # Even
-        # if even just Paste
+
         if n % len(text_list) == 0:
             copy_list = text_list.copy()
             for i in copy_list:
                 text_list.append(i)
             number_of_operations += 2
-        # Odd
-        # if odd Copy, and Paste
+
         else:
             for i in copy_list:
                 text_list.append(i)
@@ -41,5 +47,5 @@ def minOperations(n: int) -> int:
 
         if len(text_list) == n:
             return number_of_operations
-        num += 1
+
     return number_of_operations
