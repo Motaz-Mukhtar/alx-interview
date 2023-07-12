8d937969b5c18d1291d1cf4055521eb869afe77a
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-12 02:14:59 +0200

    Modify 0-pascal_triangle.py file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,19 +15,16 @@ def pascal_triangle(n):
     main_list = []
     copy_list = []
 
-    main_list.append(triangle_list.copy())
+    main_list.append(list(triangle_list))
     if n <= 0:
         return main_list
 
     for i in range(n - 1):
         triangle_list.append('0')
-        copy_list = triangle_list.copy()
+        copy_list = list(triangle_list)
         copy_list.reverse()
 
         for index in range(len(triangle_list)):
-            triangle_list[index] =
-            str(int(triangle_list[index]) + int(copy_list[index]))
-
-        main_list.append(triangle_list.copy())
-
+            triangle_list[index] += int(copy_list[index]))
+        main_list.append(list(triangle_list))
     return main_list
