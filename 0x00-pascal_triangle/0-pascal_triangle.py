46ec81fea7fb2866b6c5514485f17977c5c4ed01
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-12 00:45:00 +0000

    Fix pycodestyle at 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,18 +15,19 @@ def pascal_triangle(n):
     main_list = []
     copy_list = []
 
-    main_list.append(list(triangle_list))
+    main_list.append(triangle_list.copy())
     if n <= 0:
         return main_list
 
     for i in range(n - 1):
         triangle_list.append('0')
-        copy_list = list(triangle_list)
+        copy_list = triangle_list.copy()
         copy_list.reverse()
 
         for index in range(len(triangle_list)):
-            triangle_list[index] += int(copy_list[index])
+            triangle_list[index] =\
+             str(int(triangle_list[index]) + int(copy_list[index]))
 
-        main_list.append(list(triangle_list))
+        main_list.append(triangle_list.copy())
 
     return main_list
