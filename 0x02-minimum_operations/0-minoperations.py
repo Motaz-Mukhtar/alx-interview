244b2843e18b13a7d5124a480653d7ec741fe827
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-12 16:15:59 +0200

    Add 0x02-minimum_operations/ directory and (/0-minoperations.py, /README.md) files.

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+    Minimum Operations
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+        Calculates the fewest number of
+        operations needed to result in
+        exactly n H Characters in the file.
+    """
+    text_list = ['H']
+    copy_list = []
+    number_of_operations = 0
+
+    if n <= 0:
+        return 0
+
+    if n == 1:
+        return 0
+
+    if n == 2:
+        return 2
+
+    if n == 3:
+        return 3
+
+    num = 0
+    while True:
+        # Even
+        # if even just Paste
+        if num % 2 == 0:
+            copy_list = text_list.copy()
+            for i in copy_list:
+                text_list.append(i)
+            number_of_operations += 2
+        # Odd
+        # if odd Copy, and Paste
+        elif num % 2 == 1:
+            for i in copy_list:
+                text_list.append(i)
+            number_of_operations += 1
+
+        if n / 2 == len(text_list):
+            copy_list = text_list.copy()
+            for i in copy_list:
+                text_list.append(i)
+            number_of_operations += 2
+
+        if len(text_list) == n:
+            return number_of_operations
+        num += 1
+    return number_of_operations
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,14 @@
+# 0x02-minimum_operations
+
+In a text file, there is a single character 'H'. Your
+text editor can execute only two operations in
+this file: Copy All and Paste. Given a
+umber n, write a method that calculates the
+fewest number of operations needed to result
+in exactly n 'H' characters in the file.
+
+
+	- Prototype: def minOperations(n)
+	- Returns an integer
+	- if 'n' is impossible to achieve, return
+
