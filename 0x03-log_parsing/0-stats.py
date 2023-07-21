73b5e30485043acc250af305f4ee923ef6c9f0c8
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-21 01:49:57 +0000

    Update 0-stats.py file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,11 +14,13 @@ if __name__ == "__main__":
     count_lines = 0
     try:
         for request in sys.stdin:
-            count_lines += 1
             request_list = request.split(' ')
-            if len(request_list) == 9:
+            if len(request_list) > 2:
                 if request_list[-2] in code_stats_dict:
                     code_stats_dict[request_list[-2]] += 1
+                    count_lines += 1
+            else:
+                continue
             total_size += int(request_list[-1])
             if count_lines == 10:
                 print("File size: {}".format(total_size))
