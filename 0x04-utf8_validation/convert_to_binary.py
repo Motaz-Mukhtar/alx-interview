2b311f6e139226e1be2ae9d659106a088a23c0cd
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-28 04:28:34 +0200

    Update /convert_to_binary.py file

--- 0x04-utf8_validation/convert_to_binary.py
+++ 0x04-utf8_validation/convert_to_binary.py
@@ -6,19 +6,14 @@ import math
 
 
 def to_binary(num: int):
-    binary_list = [0, 0, 0, 0, 0, 0, 0, 0]
-    length = len(binary_list) - 1
+    binary_list = []
 
     while num != 0:
         if num % 2 == 0:
-            pass
+            binary_list.append(0)
         else:
-            binary_list[length] = 1
+            binary_list.append(1)
         num = math.trunc(num / 2)
-        length -= 1
-
-    while len(binary_list) < 8:
-        binary_list.append(0)
 
     binary_list.reverse()
     return binary_list
