52ac751f0d7a835058dca36885a853f64f3aff27
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-28 04:20:11 +0200

    Moidfy convert_to_binary.py file

--- 0x04-utf8_validation/convert_to_binary.py
+++ 0x04-utf8_validation/convert_to_binary.py
@@ -6,17 +6,19 @@ import math
 
 
 def to_binary(num: int):
-    binary_list = []
+    binary_list = [0, 0, 0, 0, 0, 0, 0, 0]
+    length = len(binary_list) - 1
 
     while num != 0:
         if num % 2 == 0:
-            binary_list.append(0)
+            pass
         else:
-            binary_list.append(1)
+            binary_list[length] = 1
         num = math.trunc(num / 2)
+        length -= 1
 
     while len(binary_list) < 8:
         binary_list.append(0)
-    binary_list.reverse()
+
     binary_list.reverse()
     return binary_list
