597fc5516bc64ceb504e1aa2ada50729edd7ab40
Motaz-Mukhtar <motazksa2@gmail.com>
2023-08-30 10:16:58 +0200

    Update 0-island_perimeter.py file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,38 +11,71 @@ def incr_perimeter(grid, row, col):
     """
     perimeter = 0
 
-    try:
-        if grid[row][col + 1] == 0:
-            perimeter += 1
-    except KeyError:
-        pass
+    if row == 0:
+        # Right Check
+        try:
+            if grid[row][col + 1] == 0:
+                perimeter += 1
+        except IndexError:
+            pass
 
-    try:
-        if grid[row][col - 1] == 0:
-            perimeter += 1
-    except KeyError:
-        pass
-
-    try:
+        # Bottom Check
         if grid[row + 1][col] == 0:
             perimeter += 1
-    except KeyError:
+
+        return perimeter
+
+    if col == 0:
+        # Right Check
+        if grid[row][col + 1] == 0:
+            perimeter += 1
+
+        # Bottom Check
+        try:
+            if grid[row + 1][col] == 0:
+                perimeter += 1
+        except IndexError:
+            pass
+
+        return perimeter
+
+    try:
+        # Right Check
+        if grid[row][col + 1] == 0:
+            perimeter += 1
+    except IndexError:
         pass
 
     try:
+        # Left Check
+        if grid[row][col - 1] == 0:
+            perimeter += 1
+    except IndexError:
+        pass
+
+    try:
+        # Bottom Check
+        if grid[row + 1][col] == 0:
+            perimeter += 1
+    except IndexError:
+        pass
+
+    try:
+        # Top Check
         if grid[row - 1][col] == 0:
             perimeter += 1
-    except KeyError:
+    except IndexError:
         pass
 
     return perimeter
+
 
 def island_perimeter(grid):
     """
         Returns the perimeter of the
         island described in 'grid'.
     """
-    perimeter = 0
+    perimeters = 0
 
     for row in range(len(grid)):
         if 1 not in grid[row]:
@@ -50,6 +83,6 @@ def island_perimeter(grid):
 
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                perimeter += incr_perimeter(grid, row, col)
+                perimeters += incr_perimeter(grid, row, col)
 
-    return perimeter
+    return perimeters
