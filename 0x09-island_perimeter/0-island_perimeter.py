070ea4ac71d876364e11863055a325d08f437282
Motaz-Mukhtar <motazksa2@gmail.com>
2023-08-31 09:51:35 +0200

    Modify 0-island_perimeter.py fiel.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,85 +4,21 @@
 """
 
 
-def incr_perimeter(grid, row, col):
-    """
-        Check horizontally/vertically for water
-        and increses perimeter.
-    """
-    perimeter = 0
-
-    if row == 0:
-        # Right Check
-        try:
-            if grid[row][col + 1] == 0:
-                perimeter += 1
-        except IndexError:
-            pass
-
-        # Bottom Check
-        if grid[row + 1][col] == 0:
-            perimeter += 1
-
-        return perimeter
-
-    if col == 0:
-        # Right Check
-        if grid[row][col + 1] == 0:
-            perimeter += 1
-
-        # Bottom Check
-        try:
-            if grid[row + 1][col] == 0:
-                perimeter += 1
-        except IndexError:
-            pass
-
-        return perimeter
-
-    try:
-        # Right Check
-        if grid[row][col + 1] == 0:
-            perimeter += 1
-    except IndexError:
-        pass
-
-    try:
-        # Left Check
-        if grid[row][col - 1] == 0:
-            perimeter += 1
-    except IndexError:
-        pass
-
-    try:
-        # Bottom Check
-        if grid[row + 1][col] == 0:
-            perimeter += 1
-    except IndexError:
-        pass
-
-    try:
-        # Top Check
-        if grid[row - 1][col] == 0:
-            perimeter += 1
-    except IndexError:
-        pass
-
-    return perimeter
-
-
 def island_perimeter(grid):
     """
         Returns the perimeter of the
         island described in 'grid'.
     """
-    perimeters = 0
+    perimeter = 0
+    size = 0
 
     for row in range(len(grid)):
-        if 1 not in grid[row]:
-            continue
-
-        for col in range(len(grid[row])):
+        for col in range(len(grid[0])):
             if grid[row][col] == 1:
-                perimeters += incr_perimeter(grid, row, col)
+                size += 1
+                if grid[row][col - 1] == 1:
+                    perimeter += 1
+                if grid[row - 1][col] == 1:
+                    perimeter += 1
 
-    return perimeters
+    return size * 4 - perimeter * 2
